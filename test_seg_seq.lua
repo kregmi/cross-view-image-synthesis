867e1c5a726cd94964af9d23f12421a06513daad
kregmi <krishna.regmi7@gmail.com>
2018-07-19 10:35:48 -0400

    Update test_seg_seq.lua

--- test_seg_seq.lua
+++ test_seg_seq.lua
@@ -10,7 +10,7 @@ util = paths.dofile('util/util.lua')
 torch.setdefaulttensortype('torch.FloatTensor')
 
 opt = {
-    DATA_ROOT = '/home/user/Documents/DATASET/dayton/AB_AsBs_3c_seg',           -- path to images (should have subfolders 'train', 'val', etc)
+    DATA_ROOT = '',           -- path to images (should have subfolders 'train', 'val', etc)
     batchSize = 4,            -- # images in batch
     loadSize = 256,           -- scale images to this size
     fineSize = 256,           --  then crop to this size
@@ -19,11 +19,11 @@ opt = {
     display_id = 200,         -- display window id.
     gpu = 1,                  -- gpu = 0 is CPU mode. gpu=X is GPU mode on GPU X
     how_many = 'all',         -- how many test images to run (set to all to run on every image found in the data/phase folder)
-    which_direction = 'BtoA', -- AtoB or BtoA
+    which_direction = 'a2g', -- a2g or g2a
     phase = 'val',            -- train, val, test ,etc
     preprocess = 'regular',   -- for special purpose preprocessing, e.g., for colorization, change this (selects preprocessing functions in util.lua)
     aspect_ratio = 1.0,       -- aspect ratio of result images
-    name = 'dayton_A2B_Seq',                -- name of experiment, selects which model to run, should generally should be passed on command line
+    name = '',                -- name of experiment, selects which model to run, should generally should be passed on command line
     input_nc = 3,             -- #  of input image channels
     output_nc = 3,            -- #  of output image channels
     serial_batches = 1,       -- if 1, takes images in order to make batches, otherwise takes them randomly
@@ -59,16 +59,16 @@ local idx_B = nil
 local input_nc = opt.input_nc
 local output_nc = opt.output_nc
 
-if opt.which_direction=='AtoB' then
+if opt.which_direction=='g2a' then
     idx_A = {1, input_nc}
-    idx_B = {input_nc+1, input_nc+output_nc}
+    --idx_B = {input_nc+1, input_nc+output_nc}
     idx_As = {input_nc+output_nc+1, 2*input_nc+output_nc}
-    idx_Bs = {2*input_nc+output_nc+1, 2*(input_nc+output_nc)}
-elseif opt.which_direction=='BtoA' then
+    --idx_Bs = {2*input_nc+output_nc+1, 2*(input_nc+output_nc)}
+elseif opt.which_direction=='a2g' then
     idx_A = {input_nc+1, input_nc+output_nc}
-    idx_B = {1, input_nc}
+    --idx_B = {1, input_nc}
     idx_As = {2*input_nc+output_nc+1, 2*(input_nc+output_nc)}
-    idx_Bs = {input_nc+output_nc+1, 2*input_nc+output_nc}    
+    --idx_Bs = {input_nc+output_nc+1, 2*input_nc+output_nc}    
 else
     error(string.format('bad direction %s',opt.which_direction))
 end
